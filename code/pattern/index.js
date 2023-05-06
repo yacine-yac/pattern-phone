e43535f50089dbd0a35687ba73cafc1994128665
yacine <lentente1958@gmail.com>
2023-05-06 23:58:12 +0100

    define currentTag and their coordinates

--- code/pattern/index.js
+++ code/pattern/index.js
@@ -1,5 +1,6 @@
 import { Line } from "../../main/line.js";
 import Calcule from "../../main/calcule.js";
+import { handlerMove,handlerStop } from "../code.js";
 /**
  * occurs when the mouse stop moving  
  */
@@ -12,33 +13,34 @@ class Pattern{
      * @param {Lines} lines 
      * @param {HTMLElement} divLocation 
      * @param {string} events 
+     * @property {HTMLElement | null} currentTag It's refer to the current HTML element Targeted
      */
     constructor(circles,lines,divLocation,events){
         this.circles=circles;
         this.lines=lines;
         this.divLocation=divLocation;
         this.events=events;
+        this.currentTag=null;
+        this.currentTagCoordinates=null;
     }
     /**
      * 
      */
-    start(e){ //console.log(e.target.getAttribute('value'))
-        const currentalue=e.target.getAttribute('value'); 
+    start(){
+        const currentalue=this.currentTag.getAttribute('value'); 
         this.circles.globalStatus=true;
         this.circles.turnCircleActive(currentalue); 
         this.lines.add(new Line(currentalue)); 
-        this.lines.current.create();  console.log("eee",this.lines);
-        document.addEventListener(this.events.move,(t)=>this.move(t.target)); 
-        // document.addEventListener(this.events.end,()=>this.stop()); 
+        this.lines.current.create();   
     }
     /**
      * declenched when the mouse start moving
      * @param {MouseEvent} t event parameter 
      * @return void
      */
-    move(currentItem){//console.log(currentItem);
-        const currentvalue =currentItem.getAttribute('value');
-        if(currentItem.nodeName==="circle" && this.circles.circles[currentvalue].status===false){ 
+    move(){
+        const currentvalue =this.currentTag.getAttribute('value'); 
+        if(this.currentTag.nodeName==="circle" && this.circles.circles[currentvalue].status===false){ 
                 this.circles.turnCircleActive(currentvalue);   
                 let p1=[this.circles.currentCircle["x"],this.circles.currentCircle["y"]];
                 let p2=[this.circles.precendentCircle['x'],this.circles.precendentCircle["y"]];
@@ -46,13 +48,13 @@ class Pattern{
                             .setLinePosition(new Calcule(p1,p2));  
                 this.lines.add(new Line(currentvalue)); 
                 this.lines.current.create(); 
-        }else{ 
-            let p1=[currentItem.clientX,currentItem.clientY];   
+        }else{
+            // let p1=[currentItem.clientX,currentItem.clientY];   
             let p2=[this.circles.currentCircle["x"]+this.divLocation.getBoundingClientRect().left,
             this.circles.currentCircle['y']+this.divLocation.getBoundingClientRect().top]; 
-            console.log(currentItem.clientX);
-                this.lines.current.calculPosition(this.circles.currentCircle)
-                            .setLinePosition(new Calcule(p1,p2));
+//    console.log(this.currentTag,'g',this.currentTagCoordinates )
+               this.lines.current.calculPosition(this.circles.currentCircle)
+                            .setLinePosition(new Calcule(this.currentTagCoordinates,p2));
         }
     }
     /**
@@ -61,14 +63,21 @@ class Pattern{
     stop(){
 
         if(this.circles.globalStatus){   
-            document.removeEventListener(this.events.move,(t)=>this.move(t)); 
+            this.divLocation.removeEventListener(this.events.move,handlerMove); 
             setTimeout(()=>{
                       this.circles.turnCirclesOff();  
                       this.circles.globalStatus=false;
                       this.lines.clear();
-                      document.removeEventListener(this.events.start,()=>this.stop()); 
+                      this.divLocation.removeEventListener(this.events.start,handlerStop); 
             },400);  
         } 
+    }
+    setCurrentTag(currentTag){
+            this.currentTag=currentTag;
+            return this;
+    }
+    setCurrentTagCoordinates(x,y){ 
+        this.currentTagCoordinates=[x,y];
     }
 }
 
