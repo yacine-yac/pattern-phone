f64e5670a0c06123a9f42233267596d87f150326
yacine <lentente1958@gmail.com>
2023-05-06 17:44:01 +0100

    define type property of patternEvents class

--- code/pattern/events.js
+++ code/pattern/events.js
@@ -3,6 +3,7 @@
  */
 export default class PatternEvents{
     constructor(type="mouse"){ 
+        this.type=type;
         this.start=type ==="mouse" ? "mousedown" :"touchstart" ;
         this.move=type ==="mouse" ? "mousemove" :"touchmove" ;
         this.end=type ==="mouse" ? "mouseup" :"touchend" ;
